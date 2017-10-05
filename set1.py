c308959dcd93106082febb118de84e2dc4a78200
wim glenn <wim.glenn@gmail.com>
2017-10-05 12:32:17 -0500

    Create set1.py

---
+++ set1.py
@@ -0,0 +1,6 @@
+import base64
+
+s_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+s_b64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+assert base64.encode(bytes.fromhex(s_hex)) == s_b64
